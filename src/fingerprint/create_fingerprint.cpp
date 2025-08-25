16b937b4027caa056afc494cad5cc293d3bd0ecf
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2025-08-25 23:39:08 +0300

    Compute fingerprint hashes

--- src/fingerprint/create_fingerprint.cpp
+++ src/fingerprint/create_fingerprint.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <complex.h>
+#include <unordered_map>
+#include <string>
+
+typedef unsigned long long ull;
+typedef std::unordered_map<ull, std::vector<std::pair<int, std::string>>> hash_map;
 
 const int window_size = 1024;
 const int overlap = 32;
@@ -12,10 +17,12 @@ const int num_bands = 6;
 const int bands[] = {10, 20, 40, 80, 160, 512};
 
 void fft(std::vector<cd> &a);
-void process_window(std::vector<short> window);
 
-void create_fingerprint(char *filepath)
+ull get_hash(short fr1, short fr2, short delta);
+
+hash_map create_fingerprint(char *filepath)
 {
+
     std::vector<short> samples = read_wav(filepath);
 
     int num_windows = 1 + (samples.size() - window_size) / (window_size - overlap); // ensure does not overflow
@@ -38,13 +45,14 @@ void create_fingerprint(char *filepath)
     }
 
     std::ofstream out("out.txt");
+
+    std::vector<std::vector<short>> peaks;
     for (auto it : spectrogram)
     {
-
         std::vector<std::pair<int, double>> maxes;
         for (int current_band = 0; current_band < num_bands; current_band++)
         {
-            int band_start = (current_band == 0) ? 0 : (bands[current_band - 1] + 1);
+            int band_start = (current_band == 0) ? 1 : (bands[current_band - 1] + 1);
             int band_end = bands[current_band];
 
             double max = -1e4;
@@ -68,11 +76,23 @@ void create_fingerprint(char *filepath)
         }
         avg_maxes /= (double)num_bands;
 
+        std::vector<short> peaks_slice;
+
         for (auto mx : maxes)
         {
+
             if (mx.second >= avg_maxes)
+            {
+                peaks_slice.push_back((short)mx.first);
                 it[mx.first] = mx.second;
+            }
+            else
+            {
+                peaks_slice.push_back(0);
+            }
         }
+
+        peaks.push_back(std::vector<short>(peaks_slice));
 
         for (int i = 0; i < it.size(); i++)
         {
@@ -82,8 +102,37 @@ void create_fingerprint(char *filepath)
         }
         out << '\n';
     }
+    out.close();
 
-    return;
+    const int w = 4, v_off = 2;
+    std::unordered_map<ull, std::vector<std::pair<int, std::string>>> hashes;
+
+    for (int col = 0; col < peaks.size(); col++)
+    {
+        for (int row = 0; row < peaks[col].size(); row++)
+        {
+            if (peaks[col][row] == 0)
+                continue;
+
+            for (int i = std::max(0, row - v_off); i < std::min((int)peaks[col].size(), row + v_off); i++)
+            {
+                for (int j = col + 1; j <= col + w && j <= peaks.size(); j++)
+                {
+                    if (peaks[i][j] == 0)
+                        continue;
+
+                    hashes[get_hash(peaks[col][row], peaks[i][j], i - col)].push_back({col, std::string(filepath)}); // todo Song ids
+                }
+            }
+        }
+    }
+
+    return hashes;
+}
+
+ull get_hash(short fr1, short fr2, short delta)
+{
+    return fr1 << 32 + fr2 << 16 + delta;
 }
 
 void fft(std::vector<cd> &a)
--- src/fingerprint/create_fingerprint.h
+++ src/fingerprint/create_fingerprint.h
@@ -1,3 +1,6 @@
 #pragma once
-
-void create_fingerprint(char *filepath);
+#include <unordered_map>
+#include <string>
+#include <vector>
+typedef unsigned long long ull;
+std::unordered_map<ull, std::vector<std::pair<int, std::string>>> create_fingerprint(char *filepath);
