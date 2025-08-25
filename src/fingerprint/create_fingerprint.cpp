376f2ffecb82c24b1019faf26e593268d15c4604
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2025-08-25 06:40:23 +0300

    Implement FFT and spectrogram generation for fingerprint

--- src/fingerprint/create_fingerprint.cpp
+++ src/fingerprint/create_fingerprint.cpp
@@ -1,10 +1,15 @@
 #include <preprocessing/wav_reader.h>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <complex.h>
 
 const int window_size = 1024;
 const int overlap = 32;
+using cd = std::complex<double>;
+const double PI = acos(-1);
 
+void fft(std::vector<cd> &a);
 void process_window(std::vector<short> window);
 
 void create_fingerprint(char *filepath)
@@ -13,25 +18,59 @@ void create_fingerprint(char *filepath)
 
     int num_windows = 1 + (samples.size() - window_size) / (window_size - overlap); // ensure does not overflow
 
-    std::cout << samples.size() << '\n';
-    std::cout << num_windows << '\n';
+    std::vector<std::vector<double>> spectrogram;
 
     for (int i = 0; i < num_windows; i++)
     {
-        std::vector<short> current_window;
-        for (int j = i * (window_size - overlap); j < (i + 1) * (window_size - overlap); j++)
+        std::vector<cd> current_window;
+        for (int j = i * (window_size - overlap); j < i * (window_size - overlap) + window_size; j++)
         {
-            current_window.push_back(samples[j]);
+            current_window.push_back(cd((double)samples[j], 0));
         }
+        fft(current_window);
+        std::vector<double> magnitudes;
+        for (auto &fr : current_window)
+            magnitudes.push_back(std::abs(fr));
 
-        process_window(current_window);
+        spectrogram.push_back(magnitudes);
+    }
+
+    std::ofstream out("out.txt");
+    for (auto it : spectrogram)
+    {
+        for (int i = 0; i < it.size(); i++)
+        {
+            out << it[i];
+            if (i + 1 != it.size())
+                out << ", ";
+        }
+        out << '\n';
     }
 
     return;
 }
 
-void process_window(std::vector<short> window)
+void fft(std::vector<cd> &a)
 {
+    int n = a.size();
+    if (n == 1)
+        return;
 
-    return;
+    std::vector<cd> a0(n / 2), a1(n / 2);
+    for (int i = 0; 2 * i < n; i++)
+    {
+        a0[i] = a[2 * i];
+        a1[i] = a[2 * i + 1];
+    }
+    fft(a0);
+    fft(a1);
+
+    double ang = 2 * PI / n;
+    cd w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; 2 * i < n; i++)
+    {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n / 2] = a0[i] - w * a1[i];
+        w *= wn;
+    }
 }
