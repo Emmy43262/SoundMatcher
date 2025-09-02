3075f4b1794f46cee33db35afd538daff25ca758
Emmy Rotariu <emmy43262@gmail.com>
2025-09-02 14:39:17 +0200

    Refactor of fingerprint creation

--- src/fingerprint/create_fingerprint.cpp
+++ src/fingerprint/create_fingerprint.cpp
@@ -13,27 +13,56 @@ typedef std::complex<double> cd;
 
 const int window_size = 1024;
 const int overlap = 512;
-const double PI = acos(-1);
 const int num_bands = 6;
 const int bands[] = {10, 20, 40, 80, 160, 500};
+const double PI = acos(-1);
 
 void fft(std::vector<cd> &a);
 
 ull get_hash(short fr1, short fr2, short delta);
 
+std::pair<int, double> get_band_max(std::vector<double> &magnitudes, int band)
+{
+    int band_start = (band == 0) ? 1 : (bands[band - 1] + 1);
+    int band_end = bands[band];
+
+    double max = -1e4;
+    int pos_max = 0;
+
+    for (int i = band_start; i <= band_end; i++)
+    {
+        if (magnitudes[i] > max)
+        {
+            max = magnitudes[i];
+            pos_max = i;
+        }
+    }
+
+    return {pos_max, max};
+}
+
+double get_max_average(std::vector<std::pair<int, double>> &maxes)
+{
+    double sum = 0;
+
+    for (int i = 0; i < maxes.size(); i++)
+        sum += maxes[i].second;
+
+    return sum / (double)maxes.size();
+}
+
 song_hash_map create_fingerprint(char *filepath, int song_id)
 {
-
     std::vector<short> samples = read_wav(filepath);
     double song_length = (double)samples.size() / 11025.0;
 
-    int num_windows = samples.size() / (window_size - overlap); // ensure does not overflow
-
-    std::vector<std::vector<double>> spectrogram;
+    int num_windows = samples.size() / (window_size - overlap) - 1;
 
     std::vector<double> hamming;
     for (int i = 0; i < window_size; i++)
         hamming.push_back(0.54 - 0.46 * cos(2.0 * PI * i / (window_size - 1)));
+
+    std::vector<std::vector<double>> spectrogram;
 
     for (int i = 0; i < num_windows; i++)
     {
@@ -41,100 +70,44 @@ song_hash_map create_fingerprint(char *filepath, int song_id)
         int start = i * (window_size - overlap);
 
         for (int j = start; j < start + window_size; j++)
-            current_window.push_back(cd((double)samples[j] * hamming[j - start], 0));
+            current_window.push_back(cd(1.0 * samples[j] * hamming[j - start], 0));
 
         fft(current_window);
+
         std::vector<double> magnitudes;
         for (int i = 0; i <= window_size / 2; i++)
             magnitudes.push_back(std::abs(current_window[i]));
-
         spectrogram.push_back(magnitudes);
     }
+    hamming = {};
+    samples = {};
 
-    std::ofstream out("out.txt");
-
-    // std::vector<std::vector<short>> peaks;
     std::vector<Peak> peaks;
-
+    double slice_duration = song_length / (double)spectrogram.size();
     for (int slice = 0; slice < spectrogram.size(); slice++)
     {
         std::vector<std::pair<int, double>> maxes;
         for (int current_band = 0; current_band < num_bands; current_band++)
-        {
-            int band_start = (current_band == 0) ? 1 : (bands[current_band - 1] + 1);
-            int band_end = bands[current_band];
+            maxes.push_back(get_band_max(spectrogram[slice], current_band));
 
-            double max = -1e4;
-            int pos_max = 0;
-
-            for (int i = band_start; i <= band_end; i++)
-            {
-                if (spectrogram[slice][i] > max)
-                {
-                    max = spectrogram[slice][i];
-                    pos_max = i;
-                }
-                spectrogram[slice][i] = 0;
-            }
-            maxes.push_back({pos_max, max});
-        }
-
-        double avg_maxes = 0;
-        for (auto mx : maxes)
-            avg_maxes += mx.second;
-        avg_maxes /= (double)num_bands;
+        double avg_maxes = get_max_average(maxes);
 
         for (auto mx : maxes)
-        {
             if (mx.second >= avg_maxes)
-            {
-                peaks.push_back(Peak((double)slice / spectrogram.size() * song_length, (short)mx.first));
-
-                spectrogram[slice][mx.first] = mx.second;
-            }
-        }
-
-        // peaks.push_back(std::vector<short>(peaks_slice)); // peaks[i] = a peak slice
-
-        // for (int i = 0; i < it.size(); i++)
-        //{
-        // out << it[i];
-        // if (i + 1 != it.size())
-        //     out << ", ";
-        //}
-        // out << '\n';
+                peaks.push_back(Peak(1.0 * slice * slice_duration, (short)mx.first));
     }
-    out.close();
+    spectrogram = {};
 
     const int max_delta = 10;
     song_hash_map hashes;
 
-    for (int i = 0; i < peaks.size(); i++)
-        for (int j = 1; j <= max_delta && i + j < peaks.size(); j++)
+    for (int root = 0; root < peaks.size(); root++)
+        for (int j = 1; j <= max_delta && root + j < peaks.size(); j++)
         {
-            short delta_ms = (peaks[i + j].time - peaks[i].time) * 1000.0;
-            hashes[get_hash(peaks[i].frequency, peaks[i + j].frequency, delta_ms)] = {peaks[i].time, song_id};
+            int target = root + j;
+            short delta_ms = (peaks[target].time - peaks[root].time) * 1000;
+            hashes[get_hash(peaks[root].frequency, peaks[target].frequency, delta_ms)] = {peaks[root].time, song_id};
         }
-
-    /*for (int col = 0; col < peaks.size(); col++)
-    {
-        for (int row = 0; row < num_bands; row++)
-        {
-            if (peaks[col][row] == 0)
-                continue;
-
-            for (int i = std::max(0, row - v_off); i < std::min(num_bands, row + v_off); i++)
-            {
-                for (int j = col + 1; j <= col + w && j < peaks.size(); j++)
-                {
-                    if (peaks[j][i] == 0)
-                        continue;
-
-                    hashes[get_hash(peaks[col][row], peaks[j][i], j - col)].push_back({col, song_id});
-                }
-            }
-        }
-    }*/
 
     return hashes;
 }
